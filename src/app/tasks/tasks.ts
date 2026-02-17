da5500f80cbcdd15bfdffde6488bbcefb2fbf213
Daniel <d.suszter@gmail.com>
2026-02-17 03:36:31 +0100

    Implement add task funcionality with ngModel

--- src/app/tasks/tasks.ts
+++ src/app/tasks/tasks.ts
@@ -1,17 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './tasks.html',
   styleUrl: './tasks.css',
 })
 export class Tasks {
+
   tasks = [
     'Angular tanulás',
     'Backend összekötés',
     'Szakmai gyakorlat'
   ];
+
+  newTask = '';
+
+  addTask() {
+    if (this.newTask.trim()) {
+      this.tasks.push(this.newTask);
+      this.newTask = '';
+    }
+  }
 }
